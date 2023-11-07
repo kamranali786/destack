381c7cd08e156e0bce0e8e44f6a9707b0de3ddba
kamranali786 <kali@ignite.pk>
2023-11-07 15:02:32 +0500

    update

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+// next.config.js
+
+module.exports = {
+  // Other configurations
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+};
 
 module.exports = nextConfig
